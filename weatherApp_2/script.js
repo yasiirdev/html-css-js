3e75f02a4fa01c7ab95dec90d73bc6693e7351af
yasiirdev <185803128+yasiirdev@users.noreply.github.com>
2026-07-13 21:24:25 +0500

    importing 4 function from utils/app.js

--- weatherApp_2/script.js
+++ weatherApp_2/script.js
@@ -1,7 +1,33 @@
-import { getSearchLocation, userLive_geolocation } from "./utils/app.js";
+import { browserLocationAccess, coordsToDetails, getWeatherDetails, putSearchLocation } from "./utils/app.js";
 
 class weather {
     constructor() {
-        
+        this.searchFeild = document.getElementById("location")
+        this.button = document.getElementById("submitBtu");
+        this.setupStartingEvent()
     }
+        
+
+    setupStartingEvent() {
+        this.button.addEventListener('click', () => {
+            const val = this.searchFeild.value.trim();
+            putSearchLocation(val).then(obj => {
+                coordsToDetails(obj);
+                getWeatherDetails(obj);
+            })
+               
+        })
+        
+    
+    }
+ 
+
+
+
 }
+
+window.document.addEventListener("DOMContentLoaded", () => {
+            new weather();
+})
+
+
