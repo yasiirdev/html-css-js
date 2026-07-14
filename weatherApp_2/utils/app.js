0f4d6e0cba4f88b574485425b2c47a4c3247aa6b
yasiirdev <185803128+yasiirdev@users.noreply.github.com>
2026-07-14 22:28:38 +0500

    now every location show its ful name

--- weatherApp_2/utils/app.js
+++ weatherApp_2/utils/app.js
@@ -1,24 +1,26 @@
-
 // putSearchlocaton use geoapify to get search lacation coords
 export const putSearchLocation = async (searchText) => {
+  const NOMINATIM_API = "https://nominatim.openstreetmap.org/search";
   const requestOptions = {
     method: "GET",
   };
   try {
     const resp = await fetch(
-      `https://api.geoapify.com/v1/geocode/search?text=${searchText}&apiKey=c53bd4b32be34f4e8840232a350dba5f`,
+      `${NOMINATIM_API}?format=json&q=${encodeURIComponent(searchText)}`,
       requestOptions,
     );
     if (!resp.ok) {
-        return;
+      return;
     }
 
     const data = await resp.json();
-    return {
-      lat: data.features[0].properties.lat,
-      lon: data.features[0].properties.lon,
-    };
 
+    return {
+      lat: data[0].lat,
+      lon:data[0].lon,
+      name:data[0].display_name, 
+    }
+    
 
   } catch (e) {
     console.error(e);
@@ -28,8 +30,9 @@ export const putSearchLocation = async (searchText) => {
 
 // getWeatherDetails  get a param location object that contains coords lat , lon
 export const getWeatherDetails = async (location) => {
+  
+  
   const url = `https://api.met.no/weatherapi/locationforecast/2.0/complete?lat=${location.lat}&lon=${location.lon}`;
-
   try {
     const resp = await fetch(url, {
       headers: {
@@ -39,46 +42,53 @@ export const getWeatherDetails = async (location) => {
 
     if (!resp.ok) throw new Error(resp.status);
 
-    const data = await resp.json()
-      
-    const currData = data.properties.timeseries[0].data.instant.details;
+    const data = await resp.json();
+    console.log(data);
+
+    const composeData = data.properties.timeseries[0].data.instant.details;
 
     const currUserData = {
-      temperature : currData.dew_point_temperature,
-      feelsTemperature: currData.air_temperature,
-      windSpeed: currData.wind_speed,
-      humidity: currData.relative_humidity,
-      pressure: currData.air_pressure_at_sea_level,
-      rain: currData.cloud_area_fraction,
+      temperature: composeData.dew_point_temperature,
+      feelsTemperature: composeData.air_temperature,
+      windSpeed: composeData.wind_speed,
+      humidity: composeData.relative_humidity,
+      pressure: composeData.air_pressure_at_sea_level,
+      rain: composeData.cloud_area_fraction,
     };
 
-    document.getElementById("tem").innerText = currUserData.temperature;         
-    document.querySelectorAll(".feeltem").forEach(ft => ft.innerText = currUserData.feelsTemperature);
+    document
+      .querySelectorAll(".tem")
+      .forEach((t) => (t.innerText = currUserData.temperature));
+
+    document
+      .querySelectorAll(".feeltem")
+      .forEach((ft) => (ft.innerText = currUserData.feelsTemperature));
+
+    document.getElementById("humidity").innerText = currUserData.humidity;
+    document.getElementById("windspeed").innerText = currUserData.windSpeed;
+    document.getElementById("pressure").innerText =
+      `${currUserData.pressure} hPa`;
 
     const next_hour = data.properties.timeseries[0].data.next_1_hours;
-
   } catch (e) {
     console.error(e);
   }
 };
 
-// browserLocationAccess use native api to get user browser location 
+// browserLocationAccess use native api to get user browser location
 export function browserLocationAccess() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
       (posit) => {
-        
-        
         getWeatherDetails({
           lat: posit.coords.latitude,
           lon: posit.coords.longitude,
         });
-        
-     coordsToDetails({
-       lat: posit.coords.latitude,
-       lon: posit.coords.longitude,
-     });
-        
+
+
+        putSearchLocation(`${posit.coords.latitude},${posit.coords.longitude}`).then((obj) => {
+          document.getElementById("locationName").innerText = obj.name;
+        });
       },
       (error) => {
         console.log(
@@ -92,16 +102,13 @@ export function browserLocationAccess() {
 }
 
 
+// export async function coordsToDetails(l) {
+//   const resp = await fetch(
+//     `https://api.geoapify.com/v1/geocode/search?text=${l.lat},${l.lon}&lang=en&limit=5&format=json&apiKey=c53bd4b32be34f4e8840232a350dba5f`,
+//   );
 
-export async function coordsToDetails(l) {
-  const resp = await fetch(
-    `https://api.geoapify.com/v1/geocode/search?text=${l.lat},${l.lon}&lang=en&limit=5&format=json&apiKey=c53bd4b32be34f4e8840232a350dba5f`,
-  );
+//   const data = await resp.json();
 
-  const data = await resp.json();
-
-  document.getElementById("locationName").innerText =
-    data.results[0].address_line1;
-
-}
-  
+//   document.getElementById("locationName").innerText =
+//     data.results[0].address_line1;
+// }
