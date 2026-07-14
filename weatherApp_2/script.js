c8c76aea65cf7ede5ce1801f0d5eee5011973641
yasiirdev <185803128+yasiirdev@users.noreply.github.com>
2026-07-14 22:27:52 +0500

    using the class

--- weatherApp_2/script.js
+++ weatherApp_2/script.js
@@ -1,33 +1,29 @@
-import { browserLocationAccess, coordsToDetails, getWeatherDetails, putSearchLocation } from "./utils/app.js";
+import {
+  browserLocationAccess,
+  getWeatherDetails,
+  putSearchLocation,
+} from "./utils/app.js";
 
 class weather {
-    constructor() {
-        this.searchFeild = document.getElementById("location")
-        this.button = document.getElementById("submitBtu");
-        this.setupStartingEvent()
-    }
-        
+  constructor() {
+    this.searchFeild = document.getElementById("location");
+    this.button = document.getElementById("submitBtu");
+    this.setupStartingEvent();
+  }
 
-    setupStartingEvent() {
-        this.button.addEventListener('click', () => {
-            const val = this.searchFeild.value.trim();
-            putSearchLocation(val).then(obj => {
-                coordsToDetails(obj);
-                getWeatherDetails(obj);
-            })
-               
-        })
-        
-    
-    }
- 
-
-
-
+  setupStartingEvent() {
+    browserLocationAccess()
+    this.button.addEventListener("click", (e) => {
+      e.preventDefault();
+      const val = this.searchFeild.value.trim();
+      putSearchLocation(val).then((obj) => {
+        document.getElementById("locationName").innerText = obj.name;
+        getWeatherDetails(obj);
+      });
+    });
+  }
 }
 
 window.document.addEventListener("DOMContentLoaded", () => {
-            new weather();
-})
-
-
+  new weather();
+});
